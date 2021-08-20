30486159d5cbf8ab2fe52dc2df200094ba4e977a
Grasiela Gomes <produtoradama@gmail.com>
2021-08-20 19:57:24 -0300

    add twitter funcionality and new quote btn

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //Get Quotes from local
 import { localQuotes } from "./data/quotes.js";
 
+const quotes = {
+  quoteContainer: document.querySelector('#quote-container'),
+  quoteText: document.querySelector('#quote'),
+  authorText: document.querySelector('#author'),
+  newQuoteBtn: document.querySelector('#new-quote'),
+  setAuthor: value => quotes.authorText.textContent = value || 'Anonymous',
+  setQuoteText: value => quotes.quoteText.textContent = value
+}
+
 let apiQuotes = [];
 
 //Get Random Int
@@ -13,7 +22,8 @@ function newQuote() {
   const quote = apiQuotes.length > 0 
     ? apiQuotes[getRandomInt(apiQuotes.length)] 
     : localQuotes[getRandomInt(localQuotes.length)]
-  console.log(quote);
+  quotes.setAuthor(quote.author);
+  quotes.setQuoteText(quote.text);
 }
 
 // Get Quotes From API
@@ -28,4 +38,18 @@ async function getQuotes() {
   }
 }
 
-window.onload = newQuote();
+//Tweet Quote
+const tweet =  {
+  twitterBtn: document.querySelector('#twitter'),
+  tweetQuote: () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quotes.quoteText.textContent} - ${quotes.authorText.textContent}`;
+  open(twitterUrl, '_blank');
+  }
+}
+
+//Event Listeners
+tweet.twitterBtn.addEventListener('click', tweet.tweetQuote);
+quotes.newQuoteBtn.addEventListener('click', newQuote);
+
+//On load
+getQuotes();
